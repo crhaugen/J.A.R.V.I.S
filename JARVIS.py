899ba82e3c2e4ea0d51918cba8d1feb64f8e2af8
Chyanne Haugen <chyhaugen@gmail.com>
2020-06-30 16:57:01 -0700

    cat is being annoyingr

--- JARVIS.py
+++ JARVIS.py
@@ -36,7 +36,7 @@ reddit = praw.Reddit(client_id=os.getenv('CLIENT_ID'),
                       user_agent='mytestscript/1.0')
 
 
-# runs
+# bot info runs on start
 @bot.event
 async def on_ready():
     print('Logged in as')
