44d3ee49f614b3b0b9acc10915a324478fd673b2
Chyanne Haugen <chyhaugen@gmail.com>
2020-09-16 19:21:15 -0700

    aho

--- JARVIS.py
+++ JARVIS.py
@@ -177,9 +177,7 @@ async def goldstar(context):
     else:
         goldStarTable[username] = 1
 
-    #find gif to output 
-    #how can bot output gif?
-    #print out current star count
+  
 
 
 
